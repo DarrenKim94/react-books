d786b530201e562f6e93642fab578bdb922183f2
Darren Kim <dkim555@yahoo.com>
2023-05-01 14:14:11 -0700

    create add book title bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import BookCreate from "./component/BookCreate";
+
 
 function App() {
     const [books, setBook] = useState([]);
-    
-    return <div>App</div>;
+
+    const createBook = (title) => {
+        console.log('Add', title)
+    };
+
+    return ( 
+    <div>
+        <BookCreate onCreate={createBook}/>
+    </div>
+    );
 }
 
 export default App;
