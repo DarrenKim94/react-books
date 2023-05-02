89e2c74dc2e61e7d8f329ae5163bece218cc160f
Darren Kim <dkim555@yahoo.com>
2023-05-01 17:12:17 -0700

    add book edit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import BookList from './component/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
+    const editBookById = (id, newTitle) => {
+        const updatedbooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, title: newTitle};
+            }
+
+            return book
+        });
+        setBooks(updatedbooks);
+    };
+
     const deleteBookById = (id) => {
         const updatedbooks = books.filter((book) => {
             return book.id !== id;
@@ -25,7 +36,7 @@ function App() {
 
     return ( 
     <div className='app'>
-        <BookList books={books} onDelete={deleteBookById} />
+        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
         <BookCreate onCreate={createBook}/>
     </div>
     );
