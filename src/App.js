209c654ed41812f454715b91a065d539b0d3bef5
Darren Kim <dkim555@yahoo.com>
2023-05-02 17:24:10 -0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,22 @@
-import { useEffect } from 'react';
-import './index.css';
-import { useState } from "react";
-import axios from 'axios';
-import BookCreate from "./component/BookCreate";
+import { useEffect, useContext } from 'react';
+import BookCreate from './component/BookCreate';
 import BookList from './component/BookList';
-
+import BooksContext from './context/books';
 
 function App() {
-    const [books, setBooks] = useState([]);
+  const { fetchBooks } = useContext(BooksContext);
 
-    const fetchBooks = async () => {
-        const response = await axios.get('http://localhost:3001/books/');
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
-        setBooks(response.data);
-    };
-
-    useEffect(() => {
-        fetchBooks();
-    }, []);
-
-    const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`, {title: newTitle});
-        console.log(response)
-
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return {...book, ...response.data};
-            }
-
-            return book
-        });
-        setBooks(updatedBooks);
-    };
-
-    const deleteBookById = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-
-        setBooks(updatedBooks);
-    };
-
-    const createBook = async (title) => {
-       const response = await axios.post('http://localhost:3001/books/', {
-            title
-        })
-        const updatedBooks = [ ...books, response.data];
-        setBooks(updatedBooks);
-    };
-
-    return ( 
-    <div className='app'>
-        <h1>Reading List</h1>
-        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
-        <BookCreate onCreate={createBook}/>
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
-    );
+  );
 }
 
 export default App;
